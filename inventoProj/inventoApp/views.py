6415397ab54ff47c4c66cb3b5316e2883741aa7a
Gabriel Louis Gutierrez <gabe.gutz361@gmail.com>
2024-10-29 19:02:34 +0800

    made the create and read views

--- inventoProj/inventoApp/views.py
+++ inventoProj/inventoApp/views.py
@@ -14,11 +14,31 @@ def index_view(request):
 
 # Create View
 def create_view(request):
+# this is where we want to instantiate/create an item RECORD/Object,
+# or facilitate its creation by providing the form to the user.
+
     form = ItemForm()
 
-
+    # Logic when POST
+    if request.method == 'POST':
+        form = request.POST
+        if form.is_valid():
+            form.save()
+            return redirect('item_list')
+    
+    # Logic to render the FORM for filling it up
+    # Remember that Django Views handle the logic.
+    else:
+        return render(request, 'InventoApp/item_form.html', {'form':form})
 
 
 # Read View
+def item_list_view(request):
+    items = Item_Model.objects.all()
+    return render(request, 'item_list', {'items':items})
+
+
 # Update View
+
+
 # Delete View
