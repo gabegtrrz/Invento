a438c5be32ec60dd5c37d4e5104d22c72febb761
Gabriel Louis Gutierrez <gabe.gutz361@gmail.com>
2024-10-29 20:08:03 +0800

    configured url routing

---
+++ inventoProj/inventoApp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index_view ,name='index'),
+    path('create/', views.item_create_view ,name='item_create'),
+    path('list/', views.item_list_view ,name='item_list'),
+    # 
+    path('update/<int:item_id>/', views.item_update_view ,name='item_update'),
+    path('delete/<int:item_id>/', views.item_delete_view ,name='item_delete'),
+]
