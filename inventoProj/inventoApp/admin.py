0b4b7dea38b3b72a1c92520e8c5edff26e4a63e9
Gabriel Louis Gutierrez <gabe.gutz361@gmail.com>
2024-10-30 19:33:12 +0800

    added superuser and model to admin

--- inventoProj/inventoApp/admin.py
+++ inventoProj/inventoApp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from . models import Item_Model
 
 # Register your models here.
+admin.site.register(Item_Model)
--- inventoProj/inventoApp/urls.py
+++ inventoProj/inventoApp/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.index_view ,name='index'),
     path('create/', views.item_create_view ,name='item_create'),
     path('list/', views.item_list_view ,name='item_list'),
-    # 
     path('update/<int:item_id>/', views.item_update_view ,name='item_update'),
     path('delete/<int:item_id>/', views.item_delete_view ,name='item_delete'),
 ]
