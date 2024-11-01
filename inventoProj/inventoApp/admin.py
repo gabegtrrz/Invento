802e35ab164da224edd3b64a294cd987ba86433e
Gabriel Louis Gutierrez <gabe.gutz361@gmail.com>
2024-11-02 00:01:06 +0800

    added user to admin.py

--- inventoProj/inventoApp/admin.py
+++ inventoProj/inventoApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from . models import Item_Model
+from . models import Item_Model,User
 
 # Register your models here.
 admin.site.register(Item_Model)
+admin.site.register(User)
