4456bfdb94252c47fce4ee4453cae1f46a6e87b5
Gabriel Louis Gutierrez <gabe.gutz361@gmail.com>
2024-11-01 15:26:03 +0800

    url patterns for authentication feature added

--- inventoProj/inventoApp/urls.py
+++ inventoProj/inventoApp/urls.py
@@ -3,6 +3,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view ,name='index'),
+    
+    # User Authentication Feature
+    
+    path('login/', views.login_view, name ='login'),
+    path('register/', views.register_view, name ='register'),
+    path('logout/', views.logout_view, name ='logout'),
+
+
+    # CRUD Inventory Tracking Feature
+
     path('create/', views.item_create_view ,name='item_create'),
     path('list/', views.item_list_view ,name='item_list'),
     path('update/<int:item_id>/', views.item_update_view ,name='item_update'),
