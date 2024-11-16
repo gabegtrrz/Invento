61809b60b1d57e98e93151d1e7d93419ee821e1c
Gabriel Louis Gutierrez <gabe.gutz361@gmail.com>
2024-11-16 17:09:42 +0800

    updated 'Item_Model' model. NO MIGRATONS MADE

--- inventoProj/inventoApp/models.py
+++ inventoProj/inventoApp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 # User Authentication Feature Imports
 from django.contrib.auth.models import AbstractUser
 
 
 # User Authentication Feature Models
-
 
 class User(AbstractUser):
     business_name = models.CharField(max_length=100, blank=True)
@@ -16,14 +17,46 @@ class User(AbstractUser):
 
 # CRUD Inventory Feature Models
 
-
 class Item_Model(models.Model):
-    item_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    sku = models.CharField(max_length=50, unique=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.IntegerField()
-    supplier = models.CharField(max_length=100)
+    sku = models.CharField(primary_key=True, max_length=50, unique=True)
+    price = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        validators=[MinValueValidator(Decimal('0.00'))]
+    )
+    minimum_inventory = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # Descriptive Attributes
+    unit_measure = models.CharField(max_length=50)
+    weight_grams = models.DecimalField(
+        max_digits = 10,
+        decimal_places = 5,
+        validators=[MinValueValidator(Decimal('0.00'))]
+    )
+    length_cm = models.DecimalField(
+        max_digits = 10,
+        decimal_places = 2,
+        validators=[MinValueValidator(Decimal('0.00'))]
+    )
+    width_cm = models.DecimalField(
+        max_digits = 10,
+        decimal_places = 2,
+        validators=[MinValueValidator(Decimal('0.00'))]
+    )
+    height_cm= models.DecimalField(
+        max_digits = 10,
+        decimal_places = 2,
+        validators=[MinValueValidator(Decimal('0.00'))]
+    )
+
+    def current_inventory(self):
+        return sum(inventory.quantity for inventory in self.inventoryitem_set.all())
+
+    def __str__(self):
+        return f"{self.name} ({self.sku})"
+    
 
     def __str__(self):
         return f"{self.name} | ID: {self.item_id} | SKU: {self.sku}"
