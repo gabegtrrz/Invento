fdec702db195f2a537ae6818c7f231dbb261b51a
Gabriel Louis Gutierrez <gabe.gutz361@gmail.com>
2024-11-21 21:55:08 +0800

    Updated Models (no migrations)

--- inventoProj/inventoApp/models.py
+++ inventoProj/inventoApp/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.urls import reverse
+
+# Validation
+from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
 from decimal import Decimal
 
@@ -15,11 +19,14 @@ class User(AbstractUser):
         return self.username
 
 
+
 # CRUD Inventory Feature Models
+# Reference SRS -- 7. Data Requirements
+
 
 class Item_Model(models.Model):
     name = models.CharField(max_length=100)
-    sku = models.CharField(primary_key=True, max_length=50, unique=True)
+    sku = models.CharField(max_length=50, unique=True)
     price = models.DecimalField(
         max_digits=10, 
         decimal_places=2, 
@@ -29,7 +36,7 @@ class Item_Model(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # Descriptive Attributes
-    unit_measure = models.CharField(max_length=50)
+    unit_measure = models.CharField(max_length=20)
     weight_grams = models.DecimalField(
         max_digits = 10,
         decimal_places = 5,
@@ -51,12 +58,84 @@ class Item_Model(models.Model):
         validators=[MinValueValidator(Decimal('0.00'))]
     )
 
-    def current_inventory(self):
-        return sum(inventory.quantity for inventory in self.inventoryitem_set.all())
+    def get_available_quantity(self):
+        return sum(lot.available_quantity for lot in self.lots.all())
+    
+    def get_absolute_url(self):
+        return reverse('item_detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return f"{self.name} ({self.sku})"
     
+class Lot(models.Model):
+    item = models.ForeignKey(Item_Model, on_delete=models.CASCADE, related_name='lots')
+    lot_number = models.CharField(max_length=50, unique=True)
+    received_date = models.DateField()
+    expiry_date = models.DateField(null=True, blank=True)
+    initial_quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    unit_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    @property
+    def available_quantity(self):
+        used_quantity = sum(movement.quantity for movement in self.movements.filter(movement_type='OUT'))
+        return max(self.initial_quantity - used_quantity, 0)
+
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['item', 'lot_number'], name = 'unique_item_lot')
+        ]
+        # unique_together = ['item', 'lot_number']
+        # deprecated ^
+    
+    def __str__(self):
+        return f"{self.item.name} - Lot {self.lot_number}"
+
+
+class Movement(models.Model):
+    MOVEMENT_TYPES = [
+        ('IN', 'Stock In'),
+        ('OUT', 'Stock Out'),
+        
+        # 1st Element: The actual value stored in the database. 
+        # 2nd ('Stock In'): The human-readable name displayed in forms or admin interfaces.
+    ]
+    
+
+
+    movement_id = models.BigAutoField(primary_key=True)
+    lot = models.ForeignKey(Lot, on_delete=models.CASCADE, related_name='movements')
+    movement_type = models.CharField(max_length=3, choices=MOVEMENT_TYPES)
+    quantity = models.DecimalField(
+        max_digits = 10,
+        decimal_places = 2,
+        validators=[MinValueValidator(Decimal('0.00'))]
+    )
+    date = models.DateField(auto_now_add=True)
+    performed_by = models.CharField(max_length=50)
+    purchase_price = models.DecimalField(
+        _("Purchase Price"), 
+        max_digits=10, 
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal('0.00'))]
+    )
+    sale_price = models.DecimalField(
+        _("Sale Price"), 
+        max_digits=10, 
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal('0.00'))]
+    )
+    notes = models.TextField(blank=True)
+    
+    def clean(self):
+        # evaluates if quantity user wants to move now does not exceed what's left by
+        # evaluating (total of quantity move out in the past + quantity needed now) 
+        # greater than what was quantity when first deposited.
+        if self.movement_type == 'OUT':
+            past_lot_movements = Movement.object.filter(lot = self.lot, movement_type = 'OUT')
+            total_num_past_out = sum(movement.quantity for movement in past_lot_movements)
+            if total_num_past_out + self.quantity > self.lot.initial_quantity:
+                raise ValidationError(f"Insufficient quantity in lot {self.lot.lot_number}")
 
     def __str__(self):
-        return f"{self.name} | ID: {self.item_id} | SKU: {self.sku}"
+        return f"{self.get_movement_type_display()} - {self.lot} - {self.quantity}"
